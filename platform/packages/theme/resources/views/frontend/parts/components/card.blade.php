d30cbf3dfa80815789fbb06aeb7b5a5ee3fba7a1
DonBosco Mushi <donnicholaus@gmail.com>
2025-08-18 19:48:57 +0300

    fix card full height

--- platform/packages/theme/resources/views/frontend/parts/components/card.blade.php
+++ platform/packages/theme/resources/views/frontend/parts/components/card.blade.php
@@ -1,7 +1,7 @@
 @props(['class' => ''])
 
 @php
-    $classes = app('tw.merge')->merge('rounded-lg border bg-card text-card-foreground shadow-sm flex flex-col h-full', $class);
+    $classes = app('tw.merge')->merge('rounded-lg border bg-card text-card-foreground shadow-sm flex flex-col', $class);
 @endphp
 
 <div {{ $attributes->merge(['class' => $classes]) }}>
